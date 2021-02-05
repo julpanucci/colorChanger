f2e4c6c94fe4ff43476e78dc5fd4a7a00e7b1666
Julian Panucci <julian.panucci@underarmour.com>
2021-02-05 12:59:03 -0600

    Remove carthage crap

--- Sources/colorChanger/colorChanger.swift
+++ Sources/colorChanger/colorChanger.swift
@@ -1,14 +1,10 @@
 import UIKit
-import TTTAttributedLabel
 
 
 public class ColorChanger {
 	public static let shared = ColorChanger()
 	
-	public var label = TTTAttributedLabel(frame: .zero)
-	
 	private let color = UIColor.blue
-	
 	
 	public func changeColor(view: UIView) {
 		view.backgroundColor = self.color
