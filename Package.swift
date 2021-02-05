2b609e0b329fd2db58848707728cd035bb9b5fd6
Julian Panucci <julian.panucci@underarmour.com>
2021-02-05 09:58:25 -0600

    Update sources

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "colorChanger",
-            dependencies: []),
+            dependencies: [],
+			path: "Sources"),
         .testTarget(
             name: "colorChangerTests",
             dependencies: ["colorChanger"]),
