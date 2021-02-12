675e28aeffc6f56cb845e6d907091def3b47b269
Julian Panucci <julian.panucci@underarmour.com>
2021-02-12 12:22:21 -0600

    Updates

--- Sources/colorChanger/colorChanger.swift
+++ Sources/colorChanger/colorChanger.swift
@@ -1,11 +1,7 @@
 import UIKit
-import TTTAttributedLabel  
-
 
 public class ColorChanger {
 	public static let shared = ColorChanger()
-	
-	public var label = TTTAttributedLabel(frame: .zero)
 	
 	private let color = UIColor.blue
 	
